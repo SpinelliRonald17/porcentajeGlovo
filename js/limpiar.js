ec8c5b7cc047d8482f62cbf61284191000df1f78
Ronald Spinelli <70922677+SpinelliRonald17@users.noreply.github.com>
2024-09-01 03:12:27 +0200

    Add files via upload

--- js/limpiar.js
+++ js/limpiar.js
@@ -1,8 +1,27 @@
-function myFunction() {
-    document.getElementById("myForm").reset();
-}
+function percentage_1() {
+  
+    var numero1= document.getElementById("num1").value;
+    var numero2= document.getElementById("num2").value;
+    var numero3= document.getElementById("num3").value;
+    var numero4= document.getElementById("num4").value;
+    var resut= document.getElementById("rsuma").value;
+    
+      
+    //var totalRealizado = numero1 / 100  * numero2 + parseFloat(numero3) + parseFloat(numero4 ) - parseFloat(numero1); 
+    //document.writeln(totalRealizado);
 
 
-function setTwoNumberDecimal(event) {
-    this.value = parseFloat(this.value).toFixed(2);
+   var totalRealizado = document.getElementById("rsuma").value;
+   document.getElementById("rsuma")
+   .value = numero1 / 100  * numero2 + parseFloat(numero3) + parseFloat(numero4 ) - parseFloat(numero1);
+
+    Swal.fire({
+        title: "Bien Hecho!",
+        text: "Realizado ",
+        icon: "success"
+      });
+
 }
+  
+
+  
