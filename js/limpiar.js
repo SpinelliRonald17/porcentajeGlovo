04f05dc350e9b9c7583f14516cb800d7c492be9a
Ronald Spinelli <70922677+SpinelliRonald17@users.noreply.github.com>
2024-09-02 01:59:04 +0200

    Add files via upload

--- js/limpiar.js
+++ js/limpiar.js
@@ -5,6 +5,8 @@ function percentage_1() {
   var numero3= document.getElementById("num3").value;
   var numero4= document.getElementById("num4").value;
   //var resut= document.getElementById("rsuma").value;
+
+
   
     
   //var totalRealizado = numero1 / 100  * numero2 + parseFloat(numero3) + parseFloat(numero4 ) - parseFloat(numero1); 
@@ -20,10 +22,38 @@ function percentage_1() {
 if(numero1,numero2,numero3,numero4) {
   var totalRealizado = numero1 / 100  * numero2 + parseFloat(numero3) + parseFloat(numero4 ) - parseFloat(numero1); 
 
- Swal.fire({
-  title: "Calculo Realizado",
-  text: "Total a pagar: " + totalRealizado,
-  icon: "success"
+
+Swal.fire({
+  icon: "success",
+  title: "Calculo realizado total a pagar: " + totalRealizado,
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: "Aceptar",
+  denyButtonText: `Recibo PDF`
+}).then((result) => {
+  /* Read more about isConfirmed, isDenied below */
+  if (result.isConfirmed) {
+    Swal.fire("Realizado Muchas Gracias", "", "success");
+  } else if (result.isDenied) {
+
+    var doc=new jsPDF();
+  let mensaje1=document.getElementById('num1').value;
+  let mensaje2= document.getElementById("num2").value;
+  let mensaje3= document.getElementById("num3").value;
+  let mensaje4= document.getElementById("num4").value;
+  let mensaje5= document.getElementById("current_date").value;
+  doc.text(10,20,'Suma total para resultado de pago en la quincena de Glovo App Delivery');
+  doc.text(20,40,'Total realizado quincena: ' + mensaje1 + '€');
+  doc.text(20,50,'Porcentaje: ' + mensaje2 + '%');
+  doc.text(20,60,'Efectivo tomado: ' + mensaje3 + '€');
+  doc.text(20,70,'Otros ajustes: ' + mensaje4 + '€');
+  doc.text(20,80,'Total a pagar: ' + totalRealizado + '€');
+  doc.addPage();
+  doc.text(20,20,'Mi trabajo!!');
+  doc.save('certificadoPdf.pdf');
+    //genPDF();
+    Swal.fire("Descargando PDF", "", "info");
+  }
 });
 
 }
@@ -31,8 +61,8 @@ if(numero1,numero2,numero3,numero4) {
 else {
   
 Swal.fire({
-  title: "Iportante",
-  text: "Revisar datos!",
+  title: "Importante Revisar",
+  text: "Los campos no deben estar vacios",
   icon: "error"
 });
 
@@ -41,8 +71,8 @@ Swal.fire({
   } catch (error) {
     
  Swal.fire({
-  title: "Iportante",
-  text: "Revisar datos!",
+  title: "Importante Revisar",
+  text: "Error Pagina",
   icon: "error"
 });
     
@@ -63,5 +93,24 @@ myForm.reset();
     timer: 1500
   });
   
+}
+
+function genPDF(){
+
+  var doc=new jsPDF();
+  let mensaje1=document.getElementById('num1').value;
+  let mensaje2= document.getElementById("num2").value;
+  let mensaje3= document.getElementById("num3").value;
+  let mensaje4= document.getElementById("num4").value;
+  let mensaje5= document.getElementById("current_date").value;
+  doc.text(10,20,'Suma total para resultado de pago en la quincena de Glovo App Delivery');
+  doc.text(20,40,'Total realizado quincena: ' + mensaje1 + '€');
+  doc.text(20,50,'Porcentaje: ' + mensaje2 + '%');
+  doc.text(20,60,'Efectivo tomado: ' + mensaje3 + '€');
+  doc.text(20,70,'Otros ajustes: ' + mensaje4 + '€');
+  doc.text(20,80,'Otros ajustes: ' + totalRealizado + '€');
+  doc.addPage();
+  doc.text(20,20,'Mi trabajo!!');
+  doc.save('certificadoPdf.pdf');
 }
 
